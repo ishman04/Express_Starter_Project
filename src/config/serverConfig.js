b3c1ec8fb897ec9884f276e01fa444ff983e42c3
Ishman Jeet Singh <ishmanalagh@gmail.com>
2024-08-03 15:21:13 +0530

    next codes

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 console.log(process.env.PORT)
 module.exports = {
-    PORT : process.env.PORT
+    PORT : process.env.PORT,
+    DB_URL : process.env.DB_URL
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 const express = require('express');
 
 const ServerConfig = require('./config/serverConfig');
+const connectdb = require('./config/dbConfig');
 const app = express();
 
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT,async ()=>{
+    await connectdb();
     console.log(`Server running on port ${ServerConfig.PORT}`);
 })
